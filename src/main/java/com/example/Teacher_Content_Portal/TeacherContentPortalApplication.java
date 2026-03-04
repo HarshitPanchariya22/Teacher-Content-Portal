60b9bb30b2be7242b42829f766a399e516c6ea77
neel-1414 <neelyshinde1@gmail.com>
2026-03-05 00:19:56 +0530

    first commit

---
+++ src/main/java/com/example/Teacher_Content_Portal/TeacherContentPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Teacher_Content_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherContentPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherContentPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Teacher_Content_Portal/TeacherContentPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Teacher_Content_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherContentPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
