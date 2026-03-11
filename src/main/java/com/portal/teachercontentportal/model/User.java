c63c277443a677a4a82f55a183aa548fed9ac54e
neel-1414 <neelyshinde1@gmail.com>
2026-03-11 15:30:01 +0530

    Code: Connected JPA and created User table

--- src/main/java/com/portal/teachercontentportal/model/User.java
+++ src/main/java/com/portal/teachercontentportal/model/User.java
@@ -1,6 +1,67 @@
 package com.portal.teachercontentportal.model;
-
+import jakarta.persistence.*;
+@Entity
+@Table(name = "Users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false,unique = true) // this value cannot be null and should be unique
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String role;
 
-
+    public User(String name, String password,String email,String role)
+    {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+    
+    // Getter Setter func
+    public Long getId()
+    {
+        return id;
+    }
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    public String getRole()
+    {
+        return role;
+    }
+    public void setRole(String role)
+    {
+        this.role = role;
+    }
 }
