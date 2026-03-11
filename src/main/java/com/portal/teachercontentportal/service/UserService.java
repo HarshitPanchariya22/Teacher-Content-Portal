2d12d44752c362650e52fb372f2c2421c19e6dfc
neel-1414 <neelyshinde1@gmail.com>
2026-03-11 16:59:49 +0530

    Code: added UserService - created service to handle database validation

---
+++ src/main/java/com/portal/teachercontentportal/service/UserService.java
@@ -0,0 +1,21 @@
+package com.portal.teachercontentportal.service;
+import com.portal.teachercontentportal.model.User;
+import com.portal.teachercontentportal.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+    public User RegisterUser(User user)
+    {
+        return userRepository.save(user);
+    }
+    public Optional<User> getTeacherByTeacherId(String TeacherId)
+    {
+        return userRepository.findByTeacherId(TeacherId);
+    }
+}
